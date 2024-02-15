99c33b001032590108b1ec714b85331ef41b8da0
dhirajdusane <dhirajdusane@gmail.com>
2024-02-16 01:47:16 +0530

    kundali loading

--- projects/kundali-component-demo/src/app/components/north-kundali-demo/north-kundali-demo.component.ts
+++ projects/kundali-component-demo/src/app/components/north-kundali-demo/north-kundali-demo.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
+import { KundaliNorthChartComponent, chartdata } from 'ngx-kundali-north-chart';
+import * as jsonChartdata from '../../../assets/dhiruBhai.json'
 
 @Component({
   selector: 'app-north-kundali-demo',
   standalone: true,
-  imports: [MatTabsModule,MatCardModule],
+  imports: [MatTabsModule,MatCardModule,KundaliNorthChartComponent],
   templateUrl: './north-kundali-demo.component.html',
   styleUrl: './north-kundali-demo.component.sass'
 })
 export class NorthKundaliDemoComponent {
+  staticChartData!:chartdata;
 
+  constructor(){
+    //JSON.parse()
+    this.staticChartData = jsonChartdata;// new chartdata();
+}
 }
